0ea05f902f987a45150d68fede94b2eb6d56d737
aaronmorgenegg <aaronmorgenegg@gmail.com>
2019-04-05 20:33:31 -0600

    Add gitignore, remove unnecessary gameStats parameters

--- final_project/src/stats/stats.py
+++ final_project/src/stats/stats.py
@@ -3,7 +3,7 @@ from src.stats.settings import STATE_RATIOS, STATE_DATA
 from src.stats.states import getStateRatios
 
 
-def getGameStats(game, state_data):
+def getGameStats(game):
     """
 
     :param game: Slippi game object
@@ -13,6 +13,6 @@ def getGameStats(game, state_data):
     if STATE_DATA:
         game_stats['state_data'] = getStateData(game)
 
-        if STATE_RATIOS: game_stats['state_ratios'] = getStateRatios(state_data)
+        if STATE_RATIOS: game_stats['state_ratios'] = getStateRatios(game_stats['state_data'])
 
     return game_stats
